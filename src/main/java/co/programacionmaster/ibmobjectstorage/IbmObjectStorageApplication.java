4aacc0b28460fb952d6ab11e28a507055a2cebd4
barcochrist <barcochrist@gmail.com>
2021-08-12 13:14:27 -0500

    Create basic project structure

---
+++ src/main/java/co/programacionmaster/ibmobjectstorage/IbmObjectStorageApplication.java
@@ -0,0 +1,13 @@
+package co.programacionmaster.ibmobjectstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmObjectStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmObjectStorageApplication.class, args);
+	}
+
+}
